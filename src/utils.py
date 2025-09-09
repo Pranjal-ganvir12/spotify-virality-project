97c72e9249e0006fb7cb2e329de7002dafd2260e
Pranjal-ganvir12 <pranjalmanorathganvir@gmail.com>
2025-09-08 22:49:35 -0700

    handling the file cleaning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,29 @@ import pandas as pd
 class Datahandler:
     def __init__(self, path):
         self.path = path 
+        self.df = None
         
     def load(self, cols=None):
-        pd.read_csv()
+        if cols:
+            self.df = pd.read_csv(self.path, usecols= cols)
+        else :
+            self.df = pd.read_csv(self.path)
+
+        return self.df
+    
+    def drop_missing(self,cols = None):
+        if cols:
+            self.df = self.df.dropna(subset = cols)
+        else:
+            self.df = self.df.dropna()
+
+        return self.df
+    
+    def get_df(self):
+        return self.df
+    
+    def save(self, output_path):
+        self.df.to_csv(output_path, index=False )
+
+
+
